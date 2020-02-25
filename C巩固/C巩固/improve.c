8aa21efc2d9047a74587d15d7a20e1bdc91c604a
dragon-web <1582943265@qq.com>
2020-02-25 22:57:19 +0800

    函数指针模块

--- C巩固/C巩固/improve.c
+++ C巩固/C巩固/improve.c
@@ -1,6 +1,6 @@
 #include"common.h"
 
-int totalOneBinary(int a);
+/*int totalOneBinary(int a);
 int totalOneBinary(int a)
 {
 	int count = 0;
@@ -17,9 +17,28 @@ int totalOneBinary(int a)
 
 int main()
 {
-	int a = -1;
+	int a = 1;
 	int ret = totalOneBinary(a);
 	printf("%d \n", ret);
+	system("pause");
+	return 0;
+}//如何计算一个数在内存中二进制存储的二进制中1的次数
+	char a = 128;
+	printf("%d\n", a);
+*/
+int Add(int x, int y);
+int Add(int x, int y)
+{
+	return x + y;
+}
+int main()
+{
+	//int arr[10] = { 0 };
+	//int(*p)[10] = &arr;
+	//函数指针的应用
+	int(*p)(int, int) = &Add;
+	printf("%d  \n", (*p)(2, 6));
+
 	system("pause");
 	return 0;
 }
